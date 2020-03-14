99868c902917d1c82587f3c1dfbb46d1316ae3be
heather <hjohnsick@yahoo.com>
2020-03-14 19:38:29 -0400

    TryingToMakeAnAPI

---
+++ LifeAPI/Controllers/LifeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeAPI.Controllers
+{
+
+   
+    public class LifeController : Controller
+    {
+      public async Task<IActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44302/");
+
+            var response = await client.GetAsync("api/life");
+            var result = await response.Content.ReadAsStringAsync();
+
+            return View();
+        }
+    }
+}
---
+++ LifeAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LifeAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
